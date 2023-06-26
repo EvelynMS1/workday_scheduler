00f734c833d4f54a7a40d4918a7ac99dda9ba28b
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-06-26 00:07:48 -0700

    description to readme

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# workday_scheduler
-hourly scheduler
+# workday_scheduler ![license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+The inspiration behind building this application was mainly for the convenience of a helpful application; collectively storing and the ability of writing down a set of planner activities. I built this project for refining my problem solving in that it allowed for me to use new techniques and skills in a way that would be efficient and logical. The problem that this application solves is having an advantageous feature of time blocking and color coordinating the day for planning purposes. Providing a personal resourceful tool. In the undertaking of this project, I was capable of employing DOM traversal, along with the utilization of the Window Object. As well as having an understanding of event bubbling for the use of event handling, plus the utilizing of Local Storage.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [license](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [How-to-Contribute](#how-to-contribute)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A
+
+## Credits
+
+N/A
+
+## License
+
+This project is licensed under the terms of the MIT license
+
+## Badges
+
+MIT license
+
+## Features
+
+Creates a badge for license chosen for repository
+
+## How-to-Contribute
+
+## Tests
+
+## Questions
+
+-[Github](https://github.com/EvelynMS1)
+
+-montessobalevelyn@gmail.com
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [license](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [How-to-Contribute](#how-to-contribute)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+N/A
+
+## Credits
+
+N/A
+
+## License
+
+This project is licensed under the terms of the MIT license
+
+## Badges
+
+MIT license
+
+## Features
+
+Creates a badge for license chosen for repository
+
+## How-to-Contribute
+
+## Tests
+
+## Questions
+
+-[Github](https://github.com/EvelynMS1)
+
+-montessobalevelyn@gmail.com
--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//current date display
+const $datedisplay = document.getElementById("currentDay");
+const currentdate = dayjs().format('dddd, MMMM D');
+$datedisplay.textContent=currentdate;
+//notification function of event display
+const $message = document.getElementById("message");
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var button = $(".saveBtn");
+  button.click(function () {
+    //class id or element
+    var textarea = $(this).siblings(".description").val();
+    var timeId = $(this).parent().attr("id");
+    localStorage.setItem(timeId, textarea);
+    $message.textContent= 'Appointment Added to Local Storage!';
+
+      // 
+    // };
+
+
+    console.log(textarea, timeId);
+  });
+
+  for (let index = 9; index < 18; index++) {
+    $("#hour-" + index + " .description").val(
+      localStorage.getItem("hour-" + index)
+    );
+  }
+  $(".time-block").each(function () {
+    var currenthour = dayjs().hour();
+    console.log(currenthour);
+    // console.log(typeof currenthour);
+
+    var rowtime = parseInt($(this).attr("id").split("-")[1]);
+    if (rowtime < currenthour) {
+      $(this).addClass("past");
+    } else if (rowtime === currenthour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+    console.log(typeof rowtime);
+  });
 });
+//one day planner 24 hrs
